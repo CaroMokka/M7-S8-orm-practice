af460f7ebeaec2dca1315bae298e4587a48c77e1
carolina <ci.arayagonzalez@gmail.com>
2024-11-15 17:27:50 -0300

    Add path describir, registrar, prestar

--- clases/Libro.js
+++ clases/Libro.js
@@ -3,47 +3,54 @@ import { conexion } from "../conexion.js";
 class Libro {
   constructor(id, titulo, autor, anio, disponible) {
     this._id = id,
-    this._titulo = titulo,
-    this._autor = autor,
-    this._anio = anio,
-    this._disponible = disponible;
+      this._titulo = titulo,
+      this._autor = autor,
+      this._anio = anio,
+      this._disponible = disponible;
   }
 
   async describir() {
-    await this.consultar();
+    const consulta = await this.consultar();
+    if (!consulta) {
+      return null;
+    }
     const descripcion = `Titulo: ${this._titulo}, Autor: ${this._autor}, Año: ${
       this._anio
     }, Disponible: ${this._disponible ? "Si" : "No"}`;
-    return descripcion
+
+    return descripcion;
   }
   async prestar() {
-      await this.consultar()
-      if(!this._disponible){
-          console.log("No esta disponible")
-          return false
-      }
-      const args = {
-          text: "UPDATE libros SET disponible=false WHERE id=$1 RETURNING *",
-          values: [this._id]
-      }
-      const result = await conexion.query(args)
-      console.log("Libro prestado exitosamente")
-      return result.rows
+    const consulta = await this.consultar();
+    if (!consulta) {
+      return { message: "libro no existente", code: 404 };
+    }
+    if (!this._disponible) {
+      return { message: "No esta disponible", code: 422 };
+    }
+    const args = {
+      text: "UPDATE libros SET disponible=false WHERE id=$1 RETURNING *",
+      values: [this._id],
+    };
+    const result = await conexion.query(args);
+    return { message: "Libro prestado exitosamente", code: 200 };
+
   }
   async devolver() {
-      await this.consultar()
-      if(this._disponible){
-          console.log("Imposible devolver, ya que el libro se encuentra disponible")
-          return false
-      }
-      const args = {
-          text: "UPDATE libros SET disponible=true WHERE id=$1 RETURNING *",
-          values: [this._id]
-      }
-      const res = await conexion.query(args)
-      console.log("Libro devuelto con éxito")
-      return res.rows
-
+    await this.consultar();
+    if (this._disponible) {
+      console.log(
+        "Imposible devolver, ya que el libro se encuentra disponible"
+      );
+      return false;
+    }
+    const args = {
+      text: "UPDATE libros SET disponible=true WHERE id=$1 RETURNING *",
+      values: [this._id],
+    };
+    const res = await conexion.query(args);
+    console.log("Libro devuelto con éxito");
+    return res.rows;
   }
   async registrar() {
     const args = {
@@ -53,18 +60,19 @@ class Libro {
     const result = await conexion.query(args);
     return result.rows;
   }
-  async consultar(id) {
+  async consultar() {
     const result = await conexion.query("SELECT * FROM libros WHERE id=$1", [
-      this._id,
+      this._id
     ]);
+  
     if (result.rowCount == 0) return null;
     const libro = result.rows[0];
-    console.log("Libro", libro);
+
     this._titulo = libro.titulo;
     this._autor = libro.autor;
     this._anio = libro.anio;
-    this._disponible = libro.disponible
-    //console.log(result.rows)
+    this._disponible = libro.disponible;
+    return true
   }
 
   //Accesadores y Mutadores
--- index.js
+++ index.js
@@ -1,4 +1,75 @@
 import { Libro } from "./clases/Libro.js";
+import http from "http"
+import url from "url";
+
+const port = 3000
+
+http.createServer( async (request, response) => {
+     const method = request.method;
+     const urlParsed = url.parse(request.url, true);
+     const pathname = urlParsed.pathname;
+
+   
+
+    //GET
+    if ( method == "GET" && pathname == "/libro/describir") {
+        const id = urlParsed.query.id
+        const libro = new Libro(id)
+  
+        const description = await libro.describir()
+   
+        if(!description){
+            response.writeHead(404, { "Content-Type": "application/json" })
+            return response.end(JSON.stringify({ message: "Libro no existe" }))
+        }
+        response.writeHead(200, { "Content-Type": "application/json" })
+        return response.end(JSON.stringify({message: "Datos descriptivos del libro", data: description}))
+    } 
+
+    // POST
+    if(method == "POST" && pathname == "/libro/prestar"){
+        let body = ""
+        request.on("data", (chunk) => {
+            body += chunk.toString()
+        })
+        request.on("end", async () => {
+            body = JSON.parse(body)
+            const libro = new Libro(body.id)
+            const result = await libro.prestar()
+            response.setHeader("Content-Type", "application/json")
+            response.writeHead(result.code)
+            return response.end(JSON.stringify({ message: result.message }))
+        })
+  
+       
+    }
+
+    //POST - Registrar
+    if(method == "POST" && pathname == "/libro/registrar"){
+        let body = ""
+        request.on("data", (chunk)=>{
+            body += chunk.toString()
+        })
+        request.on("end", async ()=>{
+            body = JSON.parse(body)
+            
+            const libro = new Libro(null, body.titulo, body.autor, body.anio)
+            const libroRegistrado = await libro.registrar()
+
+            response.setHeader("Content-Type", "application/json")
+            response.writeHead(200)
+            return response.end(JSON.stringify({ message: "Registro de libro éxitoso" , data: libroRegistrado}))
+        })
+    }
+    
+    
+
+
+    // response.end(JSON.stringify({ message: "Ruta no existente" }))
+  })
+  .listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+  });
 
 //Ejemplo de registro de libro
 // const libro1 = new Libro(null,"El poder del ahora", "Eckhart Tolle", 1997)
@@ -31,10 +102,10 @@ import { Libro } from "./clases/Libro.js";
 // })();
 
 //Ejemplo para devolver libro
-(async() => {
-    const libro2 = new Libro(2);
-    console.log(await libro2.describir())
-    const respuesta = await libro2.devolver()
-        //console.log(respuesta);
-    console.log(await libro2.describir()) 
-  })();
+// (async() => {
+//     const libro2 = new Libro(2);
+//     console.log(await libro2.describir())
+//     const respuesta = await libro2.devolver()
+//         //console.log(respuesta);
+//     console.log(await libro2.describir()) 
+//   })();
